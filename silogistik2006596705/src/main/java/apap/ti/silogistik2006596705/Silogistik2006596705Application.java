b9cc8e09e4da3fc1c8d450c08065b0d56c124079
Shabrina Nurmalitasari <shabrinanurmalitasari@gmail.com>
2023-10-08 06:02:55 +0700

    docs: initial commit add project and gitignore

---
+++ silogistik2006596705/src/main/java/apap/ti/silogistik2006596705/Silogistik2006596705Application.java
@@ -0,0 +1,13 @@
+package apap.ti.silogistik2006596705;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Silogistik2006596705Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Silogistik2006596705Application.class, args);
+	}
+
+}
---
+++ silogistik2006596705/src/test/java/apap/ti/silogistik2006596705/Silogistik2006596705ApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.ti.silogistik2006596705;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Silogistik2006596705ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
