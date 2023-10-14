1fca95aa7527aa8e649d12b98563d11e428caa68
Shabrina Nurmalitasari <shabrina.nurmalitasari@ui.ac.id>
2023-10-14 11:36:00 +0700

    Feat/detail barang

--- silogistik2006596705/src/main/java/apap/ti/silogistik2006596705/repository/BarangDb.java
+++ silogistik2006596705/src/main/java/apap/ti/silogistik2006596705/repository/BarangDb.java
@@ -1,11 +1,13 @@
 package apap.ti.silogistik2006596705.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import apap.ti.silogistik2006596705.model.Barang;
 
 @Repository
 public interface BarangDb extends JpaRepository<Barang, String> {
-
+    @Query(value = "SELECT SUM(stok) FROM gudang_barang WHERE sku_barang=?1", nativeQuery = true)
+    int sumStokBySku(String sku);
 }
--- silogistik2006596705/src/main/java/apap/ti/silogistik2006596705/service/BarangServiceImpl.java
+++ silogistik2006596705/src/main/java/apap/ti/silogistik2006596705/service/BarangServiceImpl.java
@@ -83,4 +83,32 @@ public class BarangServiceImpl implements BarangService {
         return barang;
     }
 
+    @Override
+    public String convertTipeBarangToString(int tipeBarang) {
+        String tipeBarangString;
+
+        if (tipeBarang == 1) {
+            tipeBarangString = "Elektronik";
+        } else if (tipeBarang == 2) {
+            tipeBarangString = "Pakaian & Aksesoris";
+        } else if (tipeBarang == 3) {
+            tipeBarangString = "Makanan & Minuman";
+        } else if (tipeBarang == 4) {
+            tipeBarangString = "Kosmetik";
+        } else {
+            tipeBarangString = "Perlengkapan Rumah";
+        }
+
+        return tipeBarangString;
+    }
+
+    @Override
+    public int sumStokBySku(String sku) {
+        try {
+            return barangDb.sumStokBySku(sku);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            return 0;
+        }
+    }
 }
